2cca7cc047a5ab71c41390e8aee2d2943c6dc8ce
Arthuthu <arthurgeromello@hotmail.com>
2022-09-17 20:36:11 -0300

    * removed and fixed some errors

--- Portfolio/wwwroot/js/dark-light-mode.js
+++ Portfolio/wwwroot/js/dark-light-mode.js
@@ -1,9 +1,11 @@
 ﻿//Botão dark/light mode
 let lightToggle = document.querySelector('#lightButton');
 let body = document.querySelector('body');
-let nav = document.querySelector('nav');
+let navlinks = document.querySelector('.nav-links');
+let logo = document.querySelector('.logo');
 
 lightToggle.addEventListener('click', () => {
     document.body.classList.toggle('light');
-    document.nav.classList.toggle('light');
+    document.navlinks.classList.toggle('light');
+    document.logo.classList.toggle('light');
 });
