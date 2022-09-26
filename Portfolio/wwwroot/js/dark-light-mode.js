2a01dee2d4ca3047df59a1332f02fef2a49b78cc
Arthuthu <arthurgeromello@hotmail.com>
2022-09-26 19:43:28 -0300

    Atualizado dark-light-mode.js file

--- Portfolio/wwwroot/js/dark-light-mode.js
+++ Portfolio/wwwroot/js/dark-light-mode.js
@@ -1,7 +1,13 @@
 ﻿//Botão dark/light mode
 let lightToggle = document.querySelector('#lightButton');
 let body = document.querySelector('body');
+var theme;
 
 lightToggle.addEventListener('click', () => {
-    document.body.classList.toggle('light');
+    if (document.body.classList == 'light') {
+        document.body.classList.remove('light');   
+    } else {
+        document.body.classList.add('light');
+        theme = "light";
+    }
 });
