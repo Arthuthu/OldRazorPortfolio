10c4f7b4d92d5a84a0a1596e12be0ee665184357
Arthuthu <arthurgeromello@hotmail.com>
2022-09-26 19:13:42 -0300

    Correção do light mode

--- Portfolio/wwwroot/js/dark-light-mode.js
+++ Portfolio/wwwroot/js/dark-light-mode.js
@@ -1,11 +1,7 @@
 ﻿//Botão dark/light mode
 let lightToggle = document.querySelector('#lightButton');
 let body = document.querySelector('body');
-let navlinks = document.querySelector('.nav-links');
-let logo = document.querySelector('.logo');
 
 lightToggle.addEventListener('click', () => {
     document.body.classList.toggle('light');
-    document.navlinks.classList.toggle('light');
-    document.logo.classList.toggle('light');
 });
